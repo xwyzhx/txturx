41769d5952ef4a721372f9b4d629d983cafba6fd
Yinting Huang <yintingh@sas.upenn.edu>
2016-09-10 21:00:20 -0400

    Added Setup ViewController

--- txturx/txturx/ViewController.swift
+++ txturx/txturx/ViewController.swift
@@ -21,14 +21,15 @@ class ViewController: UIViewController {
     var timer : NSTimer!
     var currentDateTime: NSDate!
     var audioPlayer = AVAudioPlayer()
+    var timeSet = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         self.numericDisplay.text = "Don't oversleep!"
-        self.resetButton.setTitle("Reset", forState: UIControlState.Normal)
-        self.startButton.setTitle("Set", forState: UIControlState.Normal)
+//        self.resetButton.setTitle("Reset", forState: UIControlState.Normal)
+//        self.startButton.setTitle("Set", forState: UIControlState.Normal)
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,21 +47,21 @@ class ViewController: UIViewController {
             numericDisplay.text = "Now we text your ex."
             return
         }
-        if elapsedTime > 5.0 {
-            numericDisplay.text = "\(Int (remainingTime))"
-        }
+//        if elapsedTime > 5.0 {
+//            numericDisplay.text = "\(Int (remainingTime))"
+//        }
     }
     
     @IBAction func startButtonPressed(sender: AnyObject) {
+        self.numericDisplay.text = "Alarm set for: " + timeSet
+        
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "MMM dd, yyyy 'at' h:mm a"
         let strDate = dateFormatter.stringFromDate(datePicker.date)
-        self.numericDisplay.text = "Alarm set for: " + strDate
         let date = dateFormatter.dateFromString(strDate)
         
         currentDateTime = NSDate()
         let calendar = NSCalendar.currentCalendar()
-        
         let datecomponents = calendar.components(.Second, fromDate: currentDateTime, toDate: date!, options: NSCalendarOptions.WrapComponents)
         
         timeLeft = Double (datecomponents.second)
@@ -76,9 +77,9 @@ class ViewController: UIViewController {
     
     @IBAction func datePickerAction(sender: AnyObject) {
         let dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "MMM dd, yyyy 'at' h:mm a"
-        let strDate = dateFormatter.stringFromDate(datePicker.date)
-        self.numericDisplay.text = "Set alarm for \(strDate)?"
+        dateFormatter.dateFormat = "h:mm a"
+        timeSet = dateFormatter.stringFromDate(datePicker.date)
+        self.numericDisplay.text = "Set alarm for \(timeSet)?"
     }
 }
 
